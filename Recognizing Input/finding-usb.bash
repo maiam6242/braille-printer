3d0b964d8ba607be43028d720a85ed99c6d7a6ca
maiam6242 <maiam6242@gmail.com>
2019-11-19 13:32:00 -0500

    trying to make github my friend... it's rough

--- Recognizing Input/finding-usb.bash
+++ Recognizing Input/finding-usb.bash
@@ -4,14 +4,16 @@
 lsusb
 
 # get into media folder and into the directory
-cd ..
+cd 
 cd /media/pi/
+print "$(/media/pi/ |head -n1)"
 
 ls
 
+# print all of the text files
 find -type f -iname "*.txt" -printf "%f\n"
 
-for file in find -type f -iname "*.txt":
-do
-print "$file" 
-done
+# for file in find -type f -iname "*.txt":
+# do
+# print "$file" 
+# done
