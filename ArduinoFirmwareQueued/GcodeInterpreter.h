53de4f6469456b1e969144085a514fba9831a093
Colin Snow <colinmsnow@gmail.com>
2019-11-23 16:04:50 -0500

    Add timeout to serial read

---
+++ ArduinoFirmwareQueued/GcodeInterpreter.h
@@ -0,0 +1,185 @@
+class GcodeInterpreter{
+
+    protected:
+        int numSolenoids = 14;
+        int solenoidTime = 200;
+        int defaultSpeed = 8000;
+        bool machineMode = false; // Mode determines whether it is in human or machine interaction mode. Defaults to human
+
+    public:
+
+        // Execute the command which is typed into the serial monitor
+        // @args horizontalStepper The horizontal stepper object
+        // @args paperStepper The paper stepper object
+        // @args solenoid The solenoid object
+        void executeCommand(HorizontalStepper& horizontalStepper, PaperStepper& paperStepper, Solenoid& solenoid){
+            /*
+
+            Commands:
+
+            G28                     Home horizontal axis
+            M17                     Enable all steppers
+            M18                     Disable all steppers
+            M701                    Load paper
+            M702                    Unload paper
+            M21                     Toggle machine-read mode
+            M114                    Print Positions
+            G0 x val                Fast move to character on x axis
+            G0 y val                Fast move to character on y axis
+            G1 x val                Controlled move to character on x axis
+            G1 y val                Controlled move to character on y axis
+            F array(values(14))     Fire all solenoids with a 1 as their value
+
+
+            I hate all of this code please fix
+            
+            */
+
+            if (Serial.available() > 0) {
+                // read the incoming byte:
+                Serial.setTimeout(250);
+                String str = Serial.readString();
+                // if (machineMode){
+                //     Serial.println(str);
+                // }
+
+                if ( str.charAt(0) == 'M' && str.charAt(1) == '2' && str.charAt(2) == '1'){
+                    machineMode = !machineMode;
+                    if (machineMode){
+                        Serial.println('0');
+                    }
+                }
+                if ( str.charAt(0) == 'G' && str.charAt(1) == '2' && str.charAt(2) == '8'){
+                    if (!machineMode){
+                        Serial.println("Homing Horizontal Axis");
+                    }
+                    horizontalStepper.home(defaultSpeed);
+                    if (machineMode){
+                        Serial.println('0');
+                    }
+                }
+                if ( str.charAt(0) == 'M' && str.charAt(1) == '1' && str.charAt(2) == '7'){
+                    if (!machineMode){
+                        Serial.println("Enabling all steppers");
+                    }
+                    horizontalStepper.enable();
+                    paperStepper.enable();
+                    if (machineMode){
+                        Serial.println('0');
+                    }
+                }
+                if ( str.charAt(0) == 'M' && str.charAt(1) == '1' && str.charAt(2) == '8'){
+                    if (!machineMode){
+                        Serial.println("Disabling all steppers");
+                    }
+                    horizontalStepper.disable();
+                    paperStepper.disable();
+                    if (machineMode){
+                        Serial.println('0');
+                    }
+                }
+                if ( str.charAt(0) == 'M' && str.charAt(1) == '7' && str.charAt(2) == '0' && str.charAt(3) == '1'){
+                    if (!machineMode){
+                        Serial.println("Loading paper");
+                    }
+                    paperStepper.load(defaultSpeed);
+                    if (machineMode){
+                        Serial.println('0');
+                    }
+                }
+                if ( str.charAt(0) == 'M' && str.charAt(1) == '7' && str.charAt(2) == '0' && str.charAt(3) == '2'){
+                    if (!machineMode){
+                        Serial.println("Unloading paper");
+                    }
+                    paperStepper.unload(defaultSpeed);
+                    if (machineMode){
+                        Serial.println('0');
+                    }
+                }
+                if ( str.charAt(0) == 'G' && str.charAt(1) == '0'){
+                    if (str.charAt(3) == 'x'){
+                        str = str.substring(5);
+                        double value = str.toDouble();
+                        if (!machineMode){
+                            Serial.print("Going to x value: ");
+                            Serial.println(value);
+                        }
+                        horizontalStepper.goTomm(value, defaultSpeed);
+                        if (machineMode){
+                        Serial.println('0');
+                        }
+                    }
+                    if (str.charAt(3) == 'y'){
+                        str = str.substring(5);
+                        double value = str.toDouble();
+                        if (!machineMode){
+                            Serial.print("Going to y value: ");
+                            Serial.println(value);
+                        }
+                        paperStepper.goTomm(value, defaultSpeed);
+                        if (machineMode){
+                        Serial.println('0');
+                        }
+                    }
+                }
+                if ( str.charAt(0) == 'G' && str.charAt(1) == '1'){
+                    if (str.charAt(3) == 'x'){
+                        str = str.substring(5);
+                        double value = str.toDouble();
+                        if (!machineMode){
+                            Serial.print("Going to x value: ");
+                            Serial.println(value);
+                        }
+                        horizontalStepper.goTomm(value, (defaultSpeed / 8));
+                        if (machineMode){
+                            Serial.println('0');
+                        }
+                    }
+                    if (str.charAt(3) == 'y'){
+                        str = str.substring(5);
+                        double value = str.toDouble();
+                        if (!machineMode){
+                            Serial.print("Going to y value: ");
+                            Serial.println(value);
+                        }
+                        paperStepper.goTomm(value, (defaultSpeed / 8));
+                        if (machineMode){
+                            Serial.println('0');
+                        }
+                    }
+                }
+                if ( str.charAt(0) == 'F'){
+                    String commandList = str.substring(2);
+                    char solenoidFire[numSolenoids+1];
+                    commandList.toCharArray(solenoidFire, numSolenoids+1);  // Need to add one because char arrays have 0 as default last value
+                    int intFire[numSolenoids];
+                    for (int i=0; i<numSolenoids; i++){
+                        if (solenoidFire[i] == '1'){
+                            intFire[i] = 1;
+                        }
+                        else{
+                            intFire[i] = 0;
+                        }
+                    }
+                    solenoid.fire(intFire, solenoidTime, machineMode);
+                    if (machineMode){
+                        Serial.println('0');
+                    }
+                }
+                if ( str.charAt(0) == 'M' && str.charAt(1) == '1' && str.charAt(2) == '1' && str.charAt(3) == '4'){
+                    if (!machineMode){
+                        Serial.print("Position:");
+                        Serial.print(horizontalStepper.currentmm());
+                        Serial.print(",");
+                        Serial.println(paperStepper.currentmm());
+                    }
+                    if (machineMode){
+                        Serial.print("Position:");
+                        Serial.print(horizontalStepper.currentmm());
+                        Serial.print(",");
+                        Serial.println(paperStepper.currentmm());
+                    }
+                }
+            }
+        }
+};
---
+++ ArduinoFirmwareQueued/Steppers-2.h
@@ -0,0 +1,144 @@
+#include <SoftwareSerial.h>
+
+// Stepper control module v2
+// Created by Colin Snow on 10/29/19
+
+#include <AccelStepper.h>
+
+// A stepper object which has added functionality for moving to characters and homing
+class HorizontalStepper :  public AccelStepper {
+
+    protected:
+        int homePin;
+        double stepsPermm = 42. * 100. / 104.25 * 100. / 100.84; // How many steps it needs to move to move one mm  2000 * 8 / Pi * d (12 mm)
+        int stepsToFirstCharacter = 60; // Steps needed to get to origin
+        int absolutePosition = 0 ;
+        int enablePin;
+
+    public:
+        HorizontalStepper (int step, int direction, int enable, int home_pin, int maxSpeed, int accel) : AccelStepper(1, step, direction){
+            homePin = home_pin;
+            pinMode(homePin, INPUT_PULLUP);
+            // setEnablePin(enable);
+            enablePin = enable;
+            // setPinsInverted(false,false,true);
+            setMaxSpeed(double(maxSpeed));
+            setAcceleration(double(accel));
+        }
+
+        // Moves the stepper to the specified character
+        // @param desiredCharacter The character to go to
+        // @param stepsPerSecond The speed of the motor in steps/second
+        void goTomm(double desiredCharacter, int speed){
+            double desiredPosition = double(desiredCharacter) * stepsPermm;
+            setMaxSpeed(double(speed));
+            runToNewPosition(int(desiredPosition));
+        }
+
+        // Homes the horizontal axis
+        void home(int speed){
+            enable();
+            setMaxSpeed(speed);
+            while (digitalRead(homePin)){
+                int position = currentPosition();
+                runToNewPosition(position -20);
+            }
+            setMaxSpeed(speed / 2);
+            int position = currentPosition();
+            runToNewPosition(position + 40);
+            while (digitalRead(homePin)){
+                int position = currentPosition();
+                runToNewPosition(position -1);
+            }
+            setCurrentPosition(0);
+            runToNewPosition(stepsToFirstCharacter);
+            setCurrentPosition(0);
+        }
+
+        void enable(){
+            digitalWrite(enablePin, LOW);
+        }
+        void disable(){
+            digitalWrite(enablePin, HIGH);
+        }
+        double currentmm(){
+            return currentPosition() / stepsPermm;
+        }
+};
+
+// A stepper object which handles paper controls such as going to character and loading
+class PaperStepper : public AccelStepper{
+
+    protected:
+    int loadSteps = 225; // Number of steps needed to load in Paper
+    int unloadSteps = 3000; // Number of steps needed to unload Paper
+    int stepsPermm = 40; // Number of steps to move up one mm 2000 * 8 / Pi * d (12.7 mm)
+    int enablePin;
+    int homePin;
+
+    public:
+
+        PaperStepper (int step, int direction, int enable, int home,  int maxSpeed, int accel) : AccelStepper(1, step, direction){
+            // setEnablePin(enable);
+            enablePin = enable;
+            homePin = home;
+            pinMode(homePin, INPUT);
+            // setPinsInverted(false,false,true);
+            setMaxSpeed(maxSpeed);
+            setAcceleration(accel);
+            setPinsInverted(true,false,false);
+        }
+
+        // Loads paper into the machine
+        // @params stepsPerSecond The motor speed in steps/second
+        // void load(int speed){
+        //     setCurrentPosition(0);
+        //     setMaxSpeed(speed);
+        //     runToNewPosition(loadSteps);
+        //     setCurrentPosition(0);
+        // }
+        void load(int speed){
+            enable();
+            setMaxSpeed(speed);
+            while (!digitalRead(homePin)){
+                int position = currentPosition();
+                runToNewPosition(position +20);
+            }
+            setMaxSpeed(speed / 2);
+            int position = currentPosition();
+            runToNewPosition(position - 40);
+            while (!digitalRead(homePin)){
+                int position = currentPosition();
+                runToNewPosition(position +1);
+            }
+            setCurrentPosition(0);
+            runToNewPosition(loadSteps);
+            setCurrentPosition(0);
+        }
+
+        // Unloads paper from the machine
+        // @params stepsPerSecond The motor speed in steps/second
+        void unload(int speed){
+            int position = currentPosition();
+            setMaxSpeed(speed);
+            runToNewPosition(position + unloadSteps);
+        }
+
+        // Moves the stepper to the specified character
+        // @param desiredCharacter The character to go to
+        // @param stepsPerSecond The speed of the motor in steps/second
+        void goTomm(double desiredCharacter, int speed){
+            double desiredPosition = desiredCharacter * stepsPermm;
+            setMaxSpeed(speed);
+            runToNewPosition(desiredPosition);
+        }
+        void enable(){
+            digitalWrite(enablePin, LOW);
+        }
+        void disable(){
+            digitalWrite(enablePin, HIGH);
+        }
+        double currentmm(){
+            return currentPosition() / stepsPermm;
+        }
+};
