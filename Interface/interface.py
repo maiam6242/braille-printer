80b25085731bb67c15349ce9669071670e0406a8
maiam6242 <maiam6242@gmail.com>
2019-11-23 18:48:43 -0500

    Ugh my last git commit in this folder was so good. Go check it out, now it's rewritten. Bum...

--- Interface/interface.py
+++ Interface/interface.py
@@ -24,36 +24,40 @@ class Interface:
     def wait_for_print(self):
         self.start_print.wait_for_press()
     def is_sound(self):
+        print('Start Print is True')
         return bool(self.sound.is_pressed)
     def is_cancel(self):
+        print('Cancel is True')
         return bool(self.cancel.is_pressed)
     def is_play_pause(self):
+        print('Play Pause is True')
         return bool(self.play.is_pressed)
     def is_start_print(self):
+        print('Start Print is True')
         return bool(self.start_print.is_pressed)
 
 
-if __name__ == "__main__":
-    import time
-    interface = Interface()
-    while 1:
-        if(interface.is_start_print()):
-            print('Is start print %s' %interface.is_start_print())
-        if(interface.is_sound()):
-            print('Is sound %s' %interface.is_sound())
-        if(interface.is_play_pause()):
-            print('Is play pause %s' %interface.is_play_pause())
-        if(interface.is_cancel()):
-            print('Is cancel %s' %interface.is_cancel())
-        if interface.is_cancel() or interface.is_play_pause() or interface.is_start_print():
+# if __name__ == "__main__":
+#     import time
+#     interface = Interface()
+#     while 1:
+#         if(interface.is_start_print()):
+#             print('Is start print %s' %interface.is_start_print())
+#         if(interface.is_sound()):
+#             print('Is sound %s' %interface.is_sound())
+#         if(interface.is_play_pause()):
+#             print('Is play pause %s' %interface.is_play_pause())
+#         if(interface.is_cancel()):
+#             print('Is cancel %s' %interface.is_cancel())
+#         if interface.is_cancel() or interface.is_play_pause() or interface.is_start_print():
 
-            interface.signal_ready()
-            time.sleep(.5)
-            interface.resolve_ready()
-            time.sleep(.5)
+#             interface.signal_ready()
+#             time.sleep(.5)
+#             interface.resolve_ready()
+#             time.sleep(.5)
             
-            interface.signal_error()
-            time.sleep(.5)
-            interface.resolve_error()
-            time.sleep(.5)
+#             interface.signal_error()
+#             time.sleep(.5)
+#             interface.resolve_error()
+#             time.sleep(.5)
 
