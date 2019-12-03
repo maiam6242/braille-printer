556c8a654554bfa828590a7ed747cc1d3664ce68
maiam6242 <maiam6242@gmail.com>
2019-12-03 11:24:22 -0500

    Agh

--- Software/Recognizing Input/testing for funzies.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('rate', 130)
-engine.setProperty('voice', voices[15].id)
-engine.say("hello, friends!")
-engine.runAndWait()
