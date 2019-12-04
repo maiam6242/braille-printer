d6fcfc2e4d492ab537dd14e23aa3b1803a491e6c
Colin Snow <colinmsnow@gmail.com>
2019-12-04 00:03:20 -0500

    Add functionality to load files from the web interface

---
+++ Software/Recognizing Input/finding-web.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# print all of the usbs connections
+# lsusb
+
+# get into media folder and into the directory
+# cd /
+# cd /media/pi/
+cd ..
+cd WebInterface/uploads
+# cd "$(/media/pi/ |head -n1)"
+# print "$(/media/pi/ |head -n1)"
+
+# ls
+
+# print all of the text files
+find -type f -iname "*.txt" -printf "%f\n"
+./
+# for file in find -type f -iname "*.txt":
+# do
+# print "$file" 
+# done
