cf956d785865e099a316401ea6df8353db04fdc1
Colin Snow <colinmsnow@gmail.com>
2019-12-04 16:49:42 -0500

    Add simple selection of files to be printed

--- Software/Recognizing_Input/finding-usb.sh
+++ Software/Recognizing_Input/finding-usb.sh
@@ -12,7 +12,7 @@ cd /media/pi/ > /dev/null 2>&1
 # ls
 
 # print all of the text files
-find -type f -iname "*.txt" -printf "%f\n"
+find ~+ -type f -iname "*.txt"
 ./ > /dev/null 2>&1
 # for file in find -type f -iname "*.txt":
 # do
