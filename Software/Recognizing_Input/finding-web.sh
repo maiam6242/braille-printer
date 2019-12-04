89a5486663959ec083c372c1368a088a3c5d5013
Colin Snow <colinmsnow@gmail.com>
2019-12-04 16:10:14 -0500

    Add simple selection of files to be printed

--- Software/Recognizing_Input/finding-web.sh
+++ Software/Recognizing_Input/finding-web.sh
@@ -6,8 +6,8 @@
 # get into media folder and into the directory
 # cd /
 # cd /media/pi/
-cd ..
-cd WebInterface/uploads
+cd .. > /dev/null 2>&1
+cd WebInterface/uploads > /dev/null 2>&1
 # cd "$(/media/pi/ |head -n1)"
 # print "$(/media/pi/ |head -n1)"
 
@@ -15,7 +15,7 @@ cd WebInterface/uploads
 
 # print all of the text files
 find -type f -iname "*.txt" -printf "%f\n"
-./
+./ > /dev/null 2>&1
 # for file in find -type f -iname "*.txt":
 # do
 # print "$file" 
