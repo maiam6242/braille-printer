f6a98311c492c4f3413f17d358c00ae811fcc13f
Colin Snow <colinmsnow@gmail.com>
2019-12-05 18:07:08 -0500

    Edit startup script

--- Software/StartupScripts/startPrinter.sh
+++ Software/StartupScripts/startPrinter.sh
@@ -13,14 +13,14 @@ pid[1]=$!
 
 while true; do 
 
-    if ! ps -p pid[0] > /dev/null
+    if ! kill -0 $(pid[0])
     then
         cd /home/pi/Desktop/braille-printer/Software/
         python3 bp_runner.py &
         pid[0]=$!
     fi
 
-    if ! ps -p pid[1] > /dev/null
+    if ! kill -0 $(pid[1])
     then
         cd /home/pi/Desktop/braille-printer/Software/WebInterface
         python3 main.py &
@@ -28,9 +28,10 @@ while true; do
     fi
 
     # Kill the subprocesses at exit
-    trap "kill ${pid[0]} ${pid[1]}; exit 1" INT
 
 done
+
+trap "kill ${pid[0]} ${pid[1]}; exit 1" INT
 
 wait
 
