d22f329538fcf8c6857b3260b4120b5d7162889f
Colin Snow <colinmsnow@gmail.com>
2019-12-05 17:47:47 -0500

    Edit startup script

--- Software/StartupScripts/startPrinter.sh
+++ Software/StartupScripts/startPrinter.sh
@@ -3,9 +3,15 @@
 # Start braille printer script
 cd /home/pi/Desktop/braille-printer/Software/
 python3 bp_runner.py &
+pid[0]=$!
 
 # Start website code
 cd WebInterface
 python3 main.py &
+pid[1]=$!
 
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+trap "kill ${pid[0]} ${pid[1]}; exit 1" INT
+wait
+
+# trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
