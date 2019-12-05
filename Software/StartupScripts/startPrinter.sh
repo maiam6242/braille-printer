5aba0e0bd282bb61d9dfdade5c166bfc62927cd7
Colin Snow <colinmsnow@gmail.com>
2019-12-05 18:09:31 -0500

    Edit startup script

--- Software/StartupScripts/startPrinter.sh
+++ Software/StartupScripts/startPrinter.sh
@@ -10,17 +10,19 @@ cd WebInterface
 python3 main.py &
 pid[1]=$!
 
+trap "kill ${pid[0]} ${pid[1]}; exit 1" INT
+
 
 while true; do 
 
-    if ! kill -0 pid[0];
+    if ! kill -0 ${pid[0]};
     then
         cd /home/pi/Desktop/braille-printer/Software/
         python3 bp_runner.py &
         pid[0]=$!
     fi
 
-    if ! kill -0 pid[1];
+    if ! kill -0 ${pid[1]};
     then
         cd /home/pi/Desktop/braille-printer/Software/WebInterface
         python3 main.py &
@@ -31,7 +33,6 @@ while true; do
 
 done
 
-trap "kill ${pid[0]} ${pid[1]}; exit 1" INT
 
 wait
 
