61e72afcd45deb20ef506b69c255cfe628bf5e93
Colin Snow <colinmsnow@gmail.com>
2019-12-06 15:23:39 -0500

    Adjust paper home position

--- Firmware/Arduino Firmware/Steppers-2.h
+++ Firmware/Arduino Firmware/Steppers-2.h
@@ -113,7 +113,7 @@ class PaperStepper : public AccelStepper{
                 runToNewPosition(position +1);
             }
             setCurrentPosition(0);
-            runToNewPosition(loadSteps);
+            goTomm(-62, 8000);
             setCurrentPosition(0);
         }
 
